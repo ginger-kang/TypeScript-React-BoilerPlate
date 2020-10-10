687ea9f260ae0dc43d85b8b7d70ea02c90e555f1
ginger-kang <kdhoon07@gmail.com>
2020-10-10 22:38:08 +0900

    :wrench: modify @eslint version to 6.8.0 and add explicit-module-boundary-types rule

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: tomato;
+`;
 
 export default function Home() {
-  return <div>hi</div>;
+  return <HomeContainer>Hello</HomeContainer>;
 }
